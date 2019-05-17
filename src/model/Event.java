64fe67dd0b8a974d99d0686b06a65736b0c86501
Lina Salinas <linasalinasd7@gmail.com>
2019-05-17 17:33:30 -0500

    Working on model classes. Method for load files being coded

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Event {
 	
 	private Participant first;
@@ -27,5 +31,36 @@ public class Event {
 	public void setRoot(Viewer root) {
 		this.root = root;
 	}
+	
+	public void loadInformation(String path) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String line = br.readLine();
+		
+		while(line!=null) {
+			if(!line.startsWith("#")) {
+				String[] parts = line.split(",");
+				String id = parts[0];
+				String firstName = parts[1];
+				String lastName = parts[2];
+				String email = parts[3];
+				String gender = parts[4];
+				String country = parts[5];
+				String imageurl = parts[6];
+				String birthday = parts[7];
+				
+				if(gender.equals("Male")) {
+					Participant maleParticipant = new Participant (id,firstName,lastName,email,Gender.MALE,country,imageurl,birthday);
+				}
+				else {
+					Participant femaleParticipant = new Participant (id,firstName,lastName,email,Gender.FEMALE,country,imageurl,birthday);
+				}
+				
+			}
+		}
+		
+		
+		br.close();
+	}
+	
 
 }
--- src/model/Participant.java
+++ src/model/Participant.java
@@ -10,19 +10,18 @@ public class Participant {
 	private String email;
 	private Gender gender;
 	private String country;
-	private Image image;
+	private String imageurl;
 	private String birthday;
 	
 	public Participant(String id, String firstName, String lastName, String email, Gender gender, String country,
-			Image image, String birthday) {
-		super();
+			String imageurl, String birthday) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.gender = gender;
 		this.country = country;
-		this.image = image;
+		this.imageurl = imageurl;
 		this.birthday = birthday;
 	}
 
@@ -30,64 +29,31 @@ public class Participant {
 		return id;
 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
 	public String getFirstName() {
 		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
 	}
 
 	public String getLastName() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public Gender getGender() {
 		return gender;
 	}
 
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-
 	public String getCountry() {
 		return country;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public Image getImage() {
-		return image;
-	}
-
-	public void setImage(Image image) {
-		this.image = image;
+	public String getImageUrl() {
+		return imageurl;
 	}
 
 	public String getBirthday() {
 		return birthday;
 	}
-
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
-
 }
